683dd0374eb303712bf445159d41597f99d0ffba
Michael Joyner <joyner.michael@gmail.com>
2018-08-25 21:42:53 +0800

    it works

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/env", (req, res) =>
+  res.send(`The message of the day is: ${process.env.SECRET_MESSAGE}`)
+);
+
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
