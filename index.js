495f68f5474ec5b6e6ba17dc0d07f7905821ae75
Michael Joyner <joyner.michael@gmail.com>
2018-08-25 22:25:21 +0800

    make a change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello happy World!"));
 app.get("/env", (req, res) =>
   res.send(`The message of the day is: ${process.env.SECRET_MESSAGE}`)
 );
