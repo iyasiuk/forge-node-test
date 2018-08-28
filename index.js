f8a76262670e0a8e0e889addfed05d8d559ca891
Michael Joyner <joyner.michael@gmail.com>
2018-08-28 09:55:31 +0800

    dont use dotenv config

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-require("dotenv").config({ path: `/home/forge/forgenode/.env` });
+require("dotenv").config();
 
 app.get("/", (req, res) => res.send("Hello happy World!"));
 app.get("/env", (req, res) =>
